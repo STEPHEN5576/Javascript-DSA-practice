813473332826bf80c4ae4b2bab894159a431801e
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2023-12-20 16:20:41 +0530

    Your commit message here

--- DSA/app.js
+++ DSA/app.js
@@ -1,1 +1,25 @@
-const a = 20;
+// check for palandrom
+var letters = [];
+
+var word = "racecar"
+
+var rword = "";
+
+// push the elements in the letters array for pop the element
+for(var i=0; i<word.length; i++){
+ letters.push(word[i]);
+}
+
+console.log(letters);
+// pop the last element and assign to rword
+for(var i=0; i<word.length; i++){
+ rword += letters.pop();
+}
+
+console.log("rword",rword);
+
+if(word === rword){
+ console.log("It is a palandrom");
+}else{
+ console.log("it is not a palandrom");
+}
